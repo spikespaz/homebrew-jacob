6f9767cf76b27f5bf296c04312ba348107dc4729
Jacob Birkett <spikespaz@outlook.com>
2021-12-23 16:55:06 -0700

    Remove autogenerated comments libtpms

--- libtpms.rb
+++ libtpms.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Libtpms < Formula
   desc "The libtpms library provides software emulation of a Trusted Platform Module (TPM 1.2 and TPM 2.0)"
   homepage "https://github.com/stefanberger/swtpm"
@@ -8,7 +5,6 @@ class Libtpms < Formula
   sha256 "9a4d1ed07b78142c394faad1a1481771d470048f5859e80593fe42c82e5635a5"
   license :cannot_represent
 
-  # depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -16,12 +12,6 @@ class Libtpms < Formula
   depends_on "openssl@3"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-
     system "./autogen.sh", "--prefix=#{prefix}", "--with-openssl", "--with-tpm2"
     system "make"
     system "make install"
