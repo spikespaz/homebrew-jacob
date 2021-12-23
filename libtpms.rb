0d49eaa13717b5a7d4dd8ff5effb9e82d69ad2d8
Jacob Birkett <spikespaz@outlook.com>
2021-12-23 16:19:10 -0700

    Setup install commands for libtpms

--- libtpms.rb
+++ libtpms.rb
@@ -19,8 +19,12 @@ class Libtpms < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+
+    system "./autogen.sh", "--prefix=#{prefix}", "--with-openssl", "--with-tpm2"
+    system "make"
+    system "make install"
   end
 
   test do
