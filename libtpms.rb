eb95ab4889f5db28769ad84adb18e0e585a6b924
Jacob Birkett <spikespaz@outlook.com>
2021-12-23 16:16:11 -0700

    Set homepage and license for libtpms

--- libtpms.rb
+++ libtpms.rb
@@ -3,10 +3,10 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Libtpms < Formula
   desc "The libtpms library provides software emulation of a Trusted Platform Module (TPM 1.2 and TPM 2.0)"
-  homepage ""
+  homepage "https://github.com/stefanberger/swtpm"
   url "https://github.com/stefanberger/libtpms/archive/refs/tags/v0.9.1.tar.gz"
   sha256 "9a4d1ed07b78142c394faad1a1481771d470048f5859e80593fe42c82e5635a5"
-  license "NOASSERTION"
+  license :cannot_represent
 
   # depends_on "cmake" => :build
 
