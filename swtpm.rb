3d03c7398f6204b2e3da4684b9faad49a061561e
Jacob Birkett <spikespaz@outlook.com>
2021-12-23 17:42:30 -0700

    Set install commands for swtpm

--- swtpm.rb
+++ swtpm.rb
@@ -22,8 +22,12 @@ class Swtpm < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+
+    system "./autogen.sh", "--prefix=#{prefix}", "--with-openssl"
+    system "make"
+    system "make install"
   end
 
   test do
