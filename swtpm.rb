0c94f6aab7ee9c94f076eca974c5f9d56848ab89
Jacob Birkett <spikespaz@outlook.com>
2021-12-23 17:53:53 -0700

    Remove autogen comments swtpm

--- swtpm.rb
+++ swtpm.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Swtpm < Formula
   desc "Libtpms-based TPM emulator with socket, character device, and Linux CUSE interface."
   homepage ""
@@ -8,7 +5,6 @@ class Swtpm < Formula
   sha256 "bed41871ad42ec852c450f5764be36b6c16456b943b912351eca9c29ce382976"
   license "NOASSERTION"
 
-  # depends_on "cmake" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "socat" => :build
@@ -23,27 +19,12 @@ class Swtpm < Formula
   depends_on "glib"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-
     system "./autogen.sh", "--prefix=#{prefix}", "--with-openssl"
     system "make"
     system "make install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test swtpm`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
