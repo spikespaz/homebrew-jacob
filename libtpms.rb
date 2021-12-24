35006ca0b764c4ef918d5a189a381f1a06bb53ed
Jacob Birkett <spikespaz@outlook.com>
2021-12-23 17:55:10 -0700

    Fix homepage libtpms

--- libtpms.rb
+++ libtpms.rb
@@ -1,6 +1,6 @@
 class Libtpms < Formula
   desc "The libtpms library provides software emulation of a Trusted Platform Module (TPM 1.2 and TPM 2.0)"
-  homepage "https://github.com/stefanberger/swtpm"
+  homepage "https://github.com/stefanberger/libtpms"
   url "https://github.com/stefanberger/libtpms/archive/refs/tags/v0.9.1.tar.gz"
   sha256 "9a4d1ed07b78142c394faad1a1481771d470048f5859e80593fe42c82e5635a5"
   license :cannot_represent
